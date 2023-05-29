80ebf06af4916c269d54b8c39dd0a47fa9a02074
Dips2020 <dipsbhattarai2020@gmail.com>
2023-05-29 20:57:50 +0545

    learned basic events and event handlers for button clicked count and paragraph content change

---
+++ js/events_eventHandlers.js
@@ -0,0 +1,11 @@
+// --- 1⬇️
+let button = document.querySelector("#myButton");
+button.addEventListener("click", function () {
+    console.log("Button clicked");
+});
+
+// --- 2⬇️
+let paragraph = document.querySelector("#myParagraph");
+paragraph.textContent = "This line is changed with (.textContent) in JavaScript";
+
+// --- 1⬇️
