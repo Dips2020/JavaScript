f7bfbab7db9eaaf9defdc7e0f6488ce2b2612e3e
Dips2020 <dipsbhattarai2020@gmail.com>
2023-09-11 13:17:55 +0545

    learned about basic of setIterval and to close setInterval using setTimeout

--- JS/Synchronous_Asynchronous.js
+++ JS/Synchronous_Asynchronous.js
@@ -9,7 +9,7 @@
 // BasicFunction()
 
 // ------------------------------ ⬇️2⬇️ ------------------------------
-// // to_check_synchronous is calling from html button
+// //! to_check_synchronous is calling from html button
 // function to_check_synchronous() {
 //     let name = prompt("Enter your name");
 //     let address = prompt("Enter your address");
@@ -20,18 +20,30 @@
 // }
 
 // ------------------------------ ⬇️3⬇️ ------------------------------
-// little bit modified version of number 2
-function to_check_synchronous() {
-    let name = prompt("Enter your full name");
-    let address = prompt("Enter your address");
-    setTimeout(function () {
-        let Newname = `Welcome to ${address}, Mr. ${name}`;
-        document.getElementById("idSynchronousName").innerHTML = Newname;
-    }, 2000);
-    document.getElementById("idSynchronousAddress").innerHTML = address;
-}
+// //! little bit modified version of number 2
+// function to_check_synchronous() {
+//     let name = prompt("Enter your full name");
+//     let address = prompt("Enter your address");
+//     setTimeout(function () {
+//         let Newname = `Welcome to ${address}, Mr. ${name}`;
+//         document.getElementById("idSynchronousName").innerHTML = Newname;
+//     }, 2000);
+//     document.getElementById("idSynchronousAddress").innerHTML = address;
+// }
 
 // ------------------------------ ⬇️4⬇️ ------------------------------
+//! setInterval
+function to_check_synchronous() {
+    let interval = setInterval(function () {
+        console.log("Hello First");
+    }, 1000);
+    setTimeout(function () {
+        clearInterval(interval);
+        console.log("Interval is closed.");
+    }, 6000);
+    console.log("Hello Second");
+}
+
 // ------------------------------ ⬇️5⬇️ ------------------------------
 // ------------------------------ ⬇️6⬇️ ------------------------------
 // ------------------------------ ⬇️7⬇️ ------------------------------
