f6b39284dc2ba84b515bbff4c4bb02e95f6eb0d4
Dips2020 <dipsbhattarai2020@gmail.com>
2023-09-12 13:51:41 +0545

    local storage

--- JS/localStorage.js
+++ JS/localStorage.js
@@ -1,4 +1,19 @@
 // ------------------------------ ⬇️1⬇️ ------------------------------
+// Basic of localStorage
+// function LocalStorage() {
+//     let name = "DiPS";
+//     let address = "KTM";
+
+//     localStorage.setItem("Username", `${name}`);
+//     localStorage.setItem("Address", `${address}`);
+// }
+// function ClearLocalStorage() {
+//     // To clear everything in localStorage
+//     localStorage.clear();
+// }
+
+// ------------------------------ ⬇️2⬇️ ------------------------------
+// Prompt the user and display it on frontend too using getItem()
 // function LocalStorage() {
 //     let name = prompt("Enter your name");
 //     let age = prompt("Enter your age");
@@ -35,36 +50,35 @@
 //     localStorage.clear();
 // }
 
-// ------------------------------ ⬇️2⬇️ ------------------------------
-//! Same as above but removed if(){ conditions ..} and updated with document.getElementById
-function LocalStorage() {
-    let name = prompt("Enter your name");
-    let age = prompt("Enter your age");
-    let address = prompt("Enter your address");
-
-    localStorage.setItem("Username", `${name}`);
-    localStorage.setItem("Age", `${age}`);
-    localStorage.setItem("Address", `${address}`);
-
-    // calling MyDetails() inside the MyFunction()
-    MyDetails();
-}
-function MyDetails() {
-    let NewName = localStorage.getItem("Username");
-    let NewAge = localStorage.getItem("Age");
-    let NewAddress = localStorage.getItem("Address");
-
-    // for displaying in frontend
-    document.getElementById("idName").innerHTML = NewName;
-    document.getElementById("idAge").innerHTML = NewAge;
-    document.getElementById("idAddress").innerHTML = NewAddress;
-}
-function ClearLocalStorage() {
-    // To clear everything in localStorage
-    localStorage.clear();
-}
-
 // ------------------------------ ⬇️3⬇️ ------------------------------
+//! Same as above number 2 but removed if(){ conditions ..} and updated with document.getElementById
+// function LocalStorage() {
+//     let name = prompt("Enter your name");
+//     let age = prompt("Enter your age");
+//     let address = prompt("Enter your address");
+
+//     localStorage.setItem("Username", `${name}`);
+//     localStorage.setItem("Age", `${age}`);
+//     localStorage.setItem("Address", `${address}`);
+
+//     // calling MyDetails() inside the MyFunction()
+//     MyDetails();
+// }
+// function MyDetails() {
+//     let NewName = localStorage.getItem("Username");
+//     let NewAge = localStorage.getItem("Age");
+//     let NewAddress = localStorage.getItem("Address");
+
+//     // for displaying in frontend
+//     document.getElementById("idName").innerHTML = NewName;
+//     document.getElementById("idAge").innerHTML = NewAge;
+//     document.getElementById("idAddress").innerHTML = NewAddress;
+// }
+// function ClearLocalStorage() {
+//     // To clear everything in localStorage
+//     localStorage.clear();
+// }
+
 // ------------------------------ ⬇️4⬇️ ------------------------------
 // ------------------------------ ⬇️5⬇️ ------------------------------
 // ------------------------------ ⬇️6⬇️ ------------------------------
