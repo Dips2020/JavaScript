e0e8e7b69e1d37e91ab8a9e4963ba8b714b57187
Dips2020 <dipsbhattarai2020@gmail.com>
2023-09-24 12:04:52 +0545

    local storage

--- JS/localStorage.js
+++ JS/localStorage.js
@@ -51,7 +51,7 @@
 // }
 
 // ------------------------------ ⬇️3⬇️ ------------------------------
-//! Same as above number 2 but removed if(){ conditions ..} and updated with document.getElementById
+// ! Same as above number 2 but removed if(){ conditions ..} and updated with document.getElementById
 // function LocalStorage() {
 //     let name = prompt("Enter your name");
 //     let age = prompt("Enter your age");
@@ -80,6 +80,32 @@
 // }
 
 // ------------------------------ ⬇️4⬇️ ------------------------------
+// Newname, NewAge, and NewAddress are listed in a single line by taking single h1 tag id ("idHead")
+function LocalStorage() {
+    let name = prompt("Enter your name");
+    let age = prompt("Enter your age");
+    let address = prompt("Enter your address");
+
+    localStorage.setItem("Username", name);
+    localStorage.setItem("Age", age);
+    localStorage.setItem("Address", address);
+
+    // calling MyDetails() inside the MyFunction()
+    MyDetails();
+}
+function MyDetails() {
+    let NewName = localStorage.getItem("Username");
+    let NewAge = localStorage.getItem("Age");
+    let NewAddress = localStorage.getItem("Address");
+
+    // for displaying in frontend
+    document.getElementById("idHead").innerHTML = `Name: ${NewName} <br> Age: ${NewAge} <br> Address: ${NewAddress}`;
+}
+function ClearLocalStorage() {
+    // To clear everything in localStorage
+    localStorage.clear();
+}
+
 // ------------------------------ ⬇️5⬇️ ------------------------------
 // ------------------------------ ⬇️6⬇️ ------------------------------
 // ------------------------------ ⬇️7⬇️ ------------------------------
